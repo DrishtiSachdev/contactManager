fbcd4b9321b18b720cc7fd2845582fe1df8cacc4
DrishtiSachdev <67588244+DrishtiSachdev@users.noreply.github.com>
2021-07-10 17:57:10 +0530

    Update contacts.js

--- src/api/contacts.js
+++ src/api/contacts.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
 
 export default axios.create({
-    baseURL:"https://my-json-server.typicode.com/DrishtiSachdev/contactManager/db",
+    baseURL:"https://my-json-server.typicode.com/DrishtiSachdev/contactManager",
 })
